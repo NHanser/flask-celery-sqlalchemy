4d00ef109d94c883937546314a7ef6805d5097d3
NHanser <nicolas.hans@gmail.com>
2022-05-08 09:57:01 +0200

    Add auth partially. To be debugged

--- tests/test_page_urls.py
+++ tests/test_page_urls.py
@@ -12,7 +12,7 @@ def test_page_urls(client):
     assert response.status_code==200
 
     # Login as user and visit User page
-    response = client.post(url_for('user.login'), follow_redirects=True,
+    response = client.post(url_for('auth_bp.login'), follow_redirects=True,
                            data=dict(email='user@example.com', password='Password1'))
     assert response.status_code==200
     response = client.get(url_for('main.member_page'), follow_redirects=True)
@@ -27,16 +27,16 @@ def test_page_urls(client):
     assert response.status_code==200
 
     # Logout
-    response = client.get(url_for('user.logout'), follow_redirects=True)
+    response = client.get(url_for('auth_bp.logout'), follow_redirects=True)
     assert response.status_code==200
 
     # Login as admin and visit Admin page
-    response = client.post(url_for('user.login'), follow_redirects=True,
+    response = client.post(url_for('auth_bp.login'), follow_redirects=True,
                            data=dict(email='admin@example.com', password='Password1'))
     assert response.status_code==200
     response = client.get(url_for('main.admin_page'), follow_redirects=True)
     assert response.status_code==200
 
     # Logout
-    response = client.get(url_for('user.logout'), follow_redirects=True)
+    response = client.get(url_for('auth_bp.logout'), follow_redirects=True)
     assert response.status_code==200
